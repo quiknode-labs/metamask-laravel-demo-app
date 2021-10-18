51fb06f505d7a94327696f51d9350d631104f72d
Rasmus Nielsen <rasmus@easify.dk>
2021-10-18 13:20:40 +0200

    Add sign in button + web3 integration

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -6,6 +6,16 @@
             <jet-authentication-card-logo />
         </template>
 
+        <div class="text-center pt-4 pb-8 border-b border-gray-200">
+            <jet-button @click="loginWeb3">
+                Login with MetaMask
+            </jet-button>
+        </div>
+
+        <div class="py-6 text-sm text-gray-500 text-center">
+            or login with your credentials...
+        </div>
+
         <jet-validation-errors class="mb-4" />
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -52,7 +62,8 @@
     import JetCheckbox from '@/Jetstream/Checkbox.vue'
     import JetLabel from '@/Jetstream/Label.vue'
     import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
+    import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+    import Web3 from 'web3/dist/web3.min.js'
 
     export default defineComponent({
         components: {
@@ -92,6 +103,24 @@
                     .post(this.route('login'), {
                         onFinish: () => this.form.reset('password'),
                     })
+            },
+
+            async loginWeb3() {
+                if (! window.ethereum) {
+                    alert('MetaMask not detected. Please try again from a MetaMask enabled browser.')
+                }
+
+                const web3 = new Web3(window.ethereum);
+
+                const message = [
+                    "I have read and accept the terms and conditions (https://example.org/tos) of this app.",
+                    "Please sign me in!"
+                ].join("\n")
+
+                const address = (await web3.eth.requestAccounts())[0]
+                const signature = await web3.eth.personal.sign(message, address)
+
+                return useForm({ message, address, signature }).post('/login-web3')
             }
         }
     })
